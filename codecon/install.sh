3716bf87bd98eb602184e24e4dd15babd785d51e
SL8R <sl8erhw@gmail.com>
2019-08-28 16:53:07 -0700

    Added login page, updated install script, updated readme
    eslint update, install script has been updated in accordance to this so all that needs to be ran is bash install.sh
    
    update install and readme directions
    
    i have added a basic login page, going to rework with styled components for css just for easier readability but for now it should work fine so that mike and jeff can work on backend
    
    updated konva added to install.sh

--- codecon/install.sh
+++ codecon/install.sh
@@ -1,10 +1,10 @@
 #update version numbers and packages in script
 echo "checking for missing packages:"
 
-[ $(node -p "require('bootstrap/package.json').version") ! "4.3.1" ] && npm install bootstrap
-[ $(node -p "require('eslint/package.json').version") != "6.2.2" ] && npm install eslint
+[ $(node -p "require('bootstrap/package.json').version") != "4.3.1" ] && npm install bootstrap
+[ $(node -p "require('eslint/package.json').version") != "6.3.0" ] && npm install eslint
 [ $(node -p "require('eslint-plugin-react/package.json').version") != "7.14.3" ] && npm install eslint-plugin-react
-[ $(node -p "require('konva/package.json').version") != "4.0.5" ] && npm install konva
+[ $(node -p "require('konva/package.json').version") != "4.0.7" ] && npm install konva
 [ $(node -p "require('react/package.json').version") != "16.9.0" ] && npm install react
 [ $(node -p "require('react-dom/package.json').version") != "16.9.0" ] && npm install react-dom
 [ $(node -p "require('react-konva/package.json').version") != "16.9.0-0" ] && npm install react-konva
