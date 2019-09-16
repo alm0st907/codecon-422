f31cbfc3e823517d647acb908498705b43035be5
SL8R <sl8erhw@gmail.com>
2019-09-16 16:38:24 -0700

    added a home page

--- codecon/src/routes.js
+++ codecon/src/routes.js
@@ -7,6 +7,7 @@ import TestNav from "./components/admin-navbar";
 import Login from "./login";
 import UserNav from "./components/user-navbar";
 import Registration from "./register";
+import Home from "./homePage";
 //These routes will also be constant
 const Routes = () => {
   return (
@@ -15,8 +16,9 @@ const Routes = () => {
         {" "}
         {/*With a switch, it will only render ONE component*/}
         {/* Have it setup so login is first page displayed */}
-        <Route exact path="/" component={Login} />{" "}
+        <Route exact path="/" component={Home} />{" "}
         {/*exact is required as it'll only render if the path is exactly what is defined, otherwise it defaults to the notfound component (aka 404)*/}
+        <Route path="/login" component={Login} />
         <Route path="/scale" component={DefconScale} />
         <Route path="/navbar" component={TestNav} />
         <Route path="/usrnav" component={UserNav} />
