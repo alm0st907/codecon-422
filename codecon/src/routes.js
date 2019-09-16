714353a021cec08e141bcff84164717bd70982ff
SL8R <sl8erhw@gmail.com>
2019-09-16 16:15:09 -0700

    This commit holds the addition of a login/signup page, a homepage with links to the respective login/registration pages, updated routes, an issue form which has NOT been integrated into the scale yet

--- codecon/src/routes.js
+++ codecon/src/routes.js
@@ -6,6 +6,9 @@ import DefconScale from "./components/scale";
 import TestNav from "./components/admin-navbar";
 import Login from "./login";
 import UserNav from "./components/user-navbar";
+import Registration from "./register";
+import Home from "./homePage";
+import Issues from "./components/issues";
 //These routes will also be constant
 const Routes = () => {
   return (
@@ -14,12 +17,14 @@ const Routes = () => {
         {" "}
         {/*With a switch, it will only render ONE component*/}
         {/* Have it setup so login is first page displayed */}
-        <Route exact path="/" component={Login} />{" "}
-        <Route path="/register" component={Registration} />
+        <Route exact path="/" component={Home} />{" "}
         {/*exact is required as it'll only render if the path is exactly what is defined, otherwise it defaults to the notfound component (aka 404)*/}
+        <Route path="/login" component={Login} />
         <Route path="/scale" component={DefconScale} />
         <Route path="/navbar" component={TestNav} />
         <Route path="/usrnav" component={UserNav} />
+        <Route path="/issues" component={Issues} />
+        <Route path="/register" component={Registration} />
         <Route component={NotFound} />{" "}
         {/*this will return the not found statement if none of the above paths are found-ALWAYS AT BOTTOM*/}
       </Switch>
