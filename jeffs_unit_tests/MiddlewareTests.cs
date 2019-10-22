91ef7887afe95e6a1d994082911a1742de6c34e2
Jeff <jeffrey.kremer.iii@gmail.com>
2019-10-21 21:38:38 -0700

    garrett unfucked my tests
    he unfucked his too

---
+++ jeffs_unit_tests/MiddlewareTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using api;
+
+namespace Tests
+{
+    public class MiddlewareTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
