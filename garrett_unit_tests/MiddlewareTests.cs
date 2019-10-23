5a5837613dcfde6d4971e565a424994993fac71e
Garrett Rudisill <garrett.rudisill@gmail.com>
2019-10-22 20:34:48 -0700

    Test GetProject Done #48

--- garrett_unit_tests/MiddlewareTests.cs
+++ garrett_unit_tests/MiddlewareTests.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
 using api.Controllers;
+using Newtonsoft.Json.Linq;
 
 namespace Tests
 {
@@ -15,7 +16,26 @@ namespace Tests
         [Test]
         public void testGetProject()
         {
-            Assert.AreEqual(ControllerUnderTest.Index(), "oops! something went wrong!\n");
+            var testReturn = ControllerUnderTest.GetProject("Codecon");
+            JObject expectedReturn = new JObject
+            {
+                {"projectName","Codecon" },
+                {"defconScale",5 },
+             {"dueDate", "9/19/2019 12:00:00 AM" }
+            };
+
+            Assert.IsNotNull(testReturn);
+
+            //Jobjects are weird for unit test
+            //date time Jvalue doesnt test well, so convert to string
+            //test areequal on string
+            //Assert.AreEqual(expectedReturn, testReturn);
+
+            Assert.AreEqual(testReturn["projectName"], expectedReturn["projectName"]);
+
+            Assert.AreEqual(testReturn["defconScale"], expectedReturn["defconScale"]);
+
+            Assert.AreEqual(testReturn["dueDate"].ToString(), expectedReturn["dueDate"].ToString());
         }
 
         [Test]
