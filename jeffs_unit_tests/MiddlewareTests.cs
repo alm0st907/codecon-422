dcf38ca08596c5f6da55f5db740e9debb07b6715
Jeff <jeffrey.kremer.iii@gmail.com>
2019-10-22 21:19:13 -0700

    Test GetTask #49 done
    Test GetTask #49 done

--- jeffs_unit_tests/MiddlewareTests.cs
+++ jeffs_unit_tests/MiddlewareTests.cs
@@ -1,17 +1,54 @@
 ﻿using NUnit.Framework;
-using api;
+using api.Controllers;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Tests
 {
     public class MiddlewareTests
     {
+        ManageProjectController ControllerUnderTest;
+
         [SetUp]
         public void Setup()
         {
+            ControllerUnderTest = new ManageProjectController();
         }
 
         [Test]
-        public void Test1()
+        public void TestGetTask()
+        {
+            var testReturn = ControllerUnderTest.GetTask("Do The Second Thing");
+
+            JObject expectedReturn = new JObject {
+                {"projectName","Codecon" },
+                {"escalationVal",2 },
+                {"assignee", "Jeff" },
+                {"description", "The thing that needs to be done second will be done second" }
+            };
+
+            //Check that the test return is not nulll
+            //then check each value to see if it is what it should be
+            Assert.IsNotNull(testReturn);
+
+            Assert.AreEqual(testReturn["projectName"], expectedReturn["projectName"]);
+
+            Assert.AreEqual(testReturn["escalationValue"], expectedReturn["escalationValue"]);
+
+            Assert.AreEqual(testReturn["assignee"], expectedReturn["assignee"]);
+
+            Assert.AreEqual(testReturn["description"], expectedReturn["description"]);
+        }
+
+        [Test]
+        public void TestCreateProject()
+        {
+
+            Assert.Pass();
+        }
+
+        [Test]
+        public void TestCreateTask()
         {
             Assert.Pass();
         }
