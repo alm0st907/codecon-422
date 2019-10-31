5f72f1d061a0a6c407d98a537f56c37a733ce1f2
Michael Berger <michael.n.berger@wsu.edu>
2019-10-30 18:45:54 -0700

    Completed backend tests. Starting on Middlewar:GetAllTasks()

---
+++ mike_unit_tests/MiddlewareTests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using api.Controllers;
+using Newtonsoft.Json.Linq;
+
+namespace Tests
+{
+    public class MiddlewareTests
+    {
+        ManageProjectController ControllerUnderTest;
+
+        [SetUp]
+        public void Setup()
+        {
+            ControllerUnderTest = new ManageProjectController();
+        }
+
+        /*
+        public JObject GetAllTasks(string projectName)
+        {
+            List<Task> retTaskList = new List<Task>();
+            JArray retJList = new JArray();
+            retTaskList = engine.GetTasks(projectName);
+
+            retJList = JArray.FromObject(retTaskList);
+            JObject finalRet = new JObject();
+            finalRet.Add("items", retJList);
+            return finalRet;
+        }
+             */
+
+        [Test]
+        public void GetAllTasks()
+        {
+
+        }
+
+    }
+}
